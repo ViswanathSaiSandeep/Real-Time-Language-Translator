20724d2c34c4f4142b38ee85d524e52fd005ff5c
Sai Sandeep <viswanathsaisandeep4029.sse@saveetha.com>
2025-08-06 09:40:52 +0530

    improved listening

--- stt.py
+++ stt.py
@@ -4,16 +4,17 @@ def speech_to_text():
     recognizer = sr.Recognizer()
     recognizer.energy_threshold = 300
     recognizer.dynamic_energy_threshold = True
-    recognizer.pause_threshold = 0.4 
+    recognizer.pause_threshold = 1.3
+    recognizer.non_speaking_duration = 1.3
     recognizer.operation_timeout = 5
 
     with sr.Microphone() as source:
         print("Calibrating mic...")
-        recognizer.adjust_for_ambient_noise(source, duration=0.2)
+        recognizer.adjust_for_ambient_noise(source, duration=1)
         while True:
             print("Listening...")
             try:
-                audio = recognizer.listen(source, timeout=2, phrase_time_limit=5)
+                audio = recognizer.listen(source, timeout=5)
             except sr.WaitTimeoutError:
                 print("No speech detected. Listening again...")
                 continue
@@ -24,6 +25,7 @@ def speech_to_text():
                 return text
             except sr.UnknownValueError:
                 print("Could not understand audio.")
+                continue
             except sr.RequestError as e:
                 print(f"API error: {e}")
             return None
