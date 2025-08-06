096a60ad869bdcb40e1600723bd2f740e3794d36
Sai Sandeep <viswanathsaisandeep4029.sse@saveetha.com>
2025-08-06 15:19:45 +0530

    added text to speech

---
+++ tts.py
@@ -0,0 +1,29 @@
+from gtts import gTTS
+import pygame
+import time
+import os
+
+def text_to_speech(text, language='en'):
+    if not text:
+        print("No text provided for TTS.")
+        return
+
+    try:
+        filename = "output.mp3"
+        tts = gTTS(text=text, lang=language)
+        tts.save(filename)
+
+        pygame.mixer.init()
+        pygame.mixer.music.load(filename)
+        time.sleep(0.3)
+        pygame.mixer.music.play()
+        print("Playing the translated speech...")
+        
+        while pygame.mixer.music.get_busy():
+            time.sleep(0.1)
+
+        pygame.mixer.quit()
+        os.remove(filename)
+
+    except Exception as e:
+        print("Error in TTS:", e)
